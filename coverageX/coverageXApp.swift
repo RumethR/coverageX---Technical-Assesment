8a9f9b6a8e1f8a4afba95d29ce5c8b4af2eebd03
RumethR <rumeth.randombage@pickme.lk>
2025-06-27 20:14:24 +0530

    Initial Commit

---
+++ coverageX/coverageXApp.swift
@@ -0,0 +1,17 @@
+//
+//  coverageXApp.swift
+//  coverageX
+//
+//  Created by Rumeth Randombage on 2025-06-27.
+//
+
+import SwiftUI
+
+@main
+struct coverageXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ coverageXTests/coverageXTests.swift
@@ -0,0 +1,17 @@
+//
+//  coverageXTests.swift
+//  coverageXTests
+//
+//  Created by Rumeth Randombage on 2025-06-27.
+//
+
+import Testing
+@testable import coverageX
+
+struct coverageXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
